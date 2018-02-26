450e12b1ce5f42db1a998d9656ea218617afffff
lihuacai168 <lihuacai168@users.noreply.github.com>
2018-02-26 16:54:44 +0800

    Update 从student表中查询年龄26~27岁的学生信息.sql
    增加year(now())获取当前年份

--- 学生和成绩/从student表中查询年龄26~27岁的学生信息.sql
+++ 学生和成绩/从student表中查询年龄26~27岁的学生信息.sql
@@ -1,2 +1,3 @@
 -- 从student表中查询年龄26~27岁的学生信息
-SELECT * FROM student WHERE 2017-birth BETWEEN 26 and 28; 
+-- SELECT * FROM student WHERE 2017-birth BETWEEN 26 and 28; 
+SELECT * FROM student WHERE year(NOW())-birth BETWEEN 26 AND 27
